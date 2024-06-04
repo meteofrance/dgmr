5ac4046690ff103c29dcd639976db7ba1571cc4c
Theo Tournier <theo.tournier@meteo.fr>
2024-06-04 14:31:12 +0000

    create $METEO_FRANCE_DATA_PATH if necessary

--- dgmr/download_data.sh
+++ dgmr/download_data.sh
@@ -2,6 +2,18 @@ if [ -f .env ]; then
     export $(grep -v '^#' .env | xargs)
 fi
 
+# Check if METEO_FRANCE_DATA_PATH is defined
+if [ -z "$METEO_FRANCE_DATA_PATH" ]; then
+    # If not, set the default path to data
+    METEO_FRANCE_DATA_PATH="data"
+fi
+
+# Create the directory if necessary
+if [ ! -d "$METEO_FRANCE_DATA_PATH" ]; then
+    mkdir -p "$METEO_FRANCE_DATA_PATH"
+    echo "Directory $METEO_FRANCE_DATA_PATH created successfully."
+fi
+
 get_rounded_date() {
     current_minutes=$(date +%M)
     rounded_minutes=$(( (current_minutes) / 5 * 5 ))
@@ -17,12 +29,12 @@ get_rounded_date() {
 METEO_FRANCE_API_URL='https://public-api.meteofrance.fr/public/DPRadar/v1/mosaiques/METROPOLE/observations/LAME_D_EAU/produit?maille=500'
 
 max_retries=20
-retry_delay=2
+retry_delay=3
 retry_count=0
 success=false
 
 while [ $retry_count -lt $max_retries ]; do
-    curl -X 'GET' $METEO_FRANCE_API_URL -H 'accept: application/octet-stream+gzip' -H "apikey: $METEO_FRANCE_API_KEY" --output "$(get_rounded_date)".h5
+    curl -X 'GET' $METEO_FRANCE_API_URL -H 'accept: application/octet-stream+gzip' -H "apikey: $METEO_FRANCE_API_KEY" --output "$METEO_FRANCE_DATA_PATH/$(get_rounded_date).h5"
     
     if [ $? -eq 0 ]; then
         success=true
@@ -34,7 +46,6 @@ while [ $retry_count -lt $max_retries ]; do
     fi
 done
 
-# Vérifier si le curl a réussi après les tentatives
 if [ "$success" = true ]; then
     echo "Download successful."
 else
